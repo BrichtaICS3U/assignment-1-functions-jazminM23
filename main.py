6d40fa34916b34f405998bc352adac06ee495a6a
JAZMIN MANNION <S199884256@OCDSB>
2018-03-28 10:54:05 -0400

    FtoC + choice

--- main.py
+++ main.py
@@ -7,11 +7,19 @@
 def CtoF (celcius):
     """Convert a given celcius temperature to its equivalent farenheit temperature."""
     f=(1.8)*celcius+32
-    return(round(f,0))
+    return int(round(f))
 
 
 ###### uncomment this when you are ready to work on it
-#def FtoC ():
+def FtoC (farenheit):
+    c=(0.55556)*(farenheit-32)
+    return int(round(c))
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(CtoF(temperature))
+conversion = int(input('Would you like to convert 1) Celcius to Farenheit, or 2) Farenheit to celcius?: '))
+if conversion == 1:
+    temperature = int(input('Enter your temperature in Celsius: '))
+    print(CtoF(temperature))
+else:
+    temperature = int(input('Enter your temperature in Farenheit: '))
+    print(FtoC(temperature))
+    
