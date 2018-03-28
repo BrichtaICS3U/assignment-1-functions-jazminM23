2440f7ef47e6a591d692051d78ef6b5682eb290c
JAZMIN MANNION <S199884256@OCDSB>
2018-03-28 10:27:06 -0400

    CtoF

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# Jazmin M
 # March 28, 2018
 
-###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+#uncomment this when you are ready to work on it
+def CtoF (celcius):
+    """Convert a given celcius temperature to its equivalent farenheit temperature."""
+    f=(1.8)*celcius+32
+    return(round(f,0))
+
 
 ###### uncomment this when you are ready to work on it
 #def FtoC ():
-#
 
 temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print(CtoF(temperature))
